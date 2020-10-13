6758a74ac4a5670e95b715604f42435548b9982f
zmpl <yiw0625@163.com>
2020-10-13 10:46:06 +0800

    创建项目

---
+++ src/main/java/com/library/library_register/LibraryRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.library.library_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryRegisterApplication.class, args);
+    }
+
+}
